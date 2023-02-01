7a8a52f3e29130d00b3c3235a59b778b4ac025d6
Dan Schoenblum <dan.schoenblum@gmail.com>
2023-01-31 23:08:25 -0800

    Add a Grid type

---
+++ grid.go
@@ -0,0 +1,61 @@
+package common
+
+type Grid[T any] struct {
+	width, height int
+	values        []T
+}
+
+func NewGrid[T any](width, height int) *Grid[T] {
+	return &Grid[T]{
+		width:  width,
+		height: height,
+		values: make([]T, width*height),
+	}
+}
+
+func (g *Grid[T]) toOffset(x, y int) int {
+	return y*g.width + x
+}
+
+func (g *Grid[T]) Width() int {
+	return g.width
+}
+
+func (g *Grid[T]) Height() int {
+	return g.height
+}
+
+func (g *Grid[T]) SetVec(pos Vec, value T) {
+	g.Set(pos.X, pos.Y, value)
+}
+
+func (g *Grid[T]) Set(x, y int, value T) {
+	g.values[g.toOffset(x, y)] = value
+}
+
+func (g *Grid[T]) GetVec(pos Vec) (value T) {
+	return g.Get(pos.X, pos.Y)
+}
+
+func (g *Grid[T]) Get(x, y int) (value T) {
+	return g.values[g.toOffset(x, y)]
+}
+
+func (g *Grid[T]) TryGetVec(pos Vec) (value T, ok bool) {
+	return g.TryGet(pos.X, pos.Y)
+}
+
+func (g *Grid[T]) TryGet(x, y int) (value T, ok bool) {
+	if !g.IsValid(x, y) {
+		return
+	}
+	return g.values[g.toOffset(x, y)], true
+}
+
+func (g *Grid[T]) IsValidVec(pos Vec) bool {
+	return g.IsValid(pos.X, pos.Y)
+}
+
+func (g *Grid[T]) IsValid(x, y int) bool {
+	return x >= 0 && x < g.width && y >= 0 && y < g.height
+}
