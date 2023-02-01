336deb4f83883080324542c9e14b1aba3e0666a6
Dan Schoenblum <dan.schoenblum@gmail.com>
2023-01-31 21:57:25 -0800

    Add Box functionality

--- box.go
+++ box.go
@@ -8,6 +8,21 @@ func NewBox(min, max Vec) Box {
 	return Box{min, max}
 }
 
+func NewBoxWithCenter(center Vec, width, height int) Box {
+	return Box{
+		NewVec(center.X-width/2, center.Y-height/2),
+		NewVec(center.X+width/2, center.Y+height/2),
+	}
+}
+
+func (b *Box) Min() Vec {
+	return b.min
+}
+
+func (b *Box) Max() Vec {
+	return b.max
+}
+
 func (b *Box) ExpandToInclude(point Vec) {
 	b.min.UpdateMin(point)
 	b.max.UpdateMax(point)
@@ -28,4 +43,37 @@ func (b Box) Height() int {
 
 func (b Box) PointAtOffset(x, y int) Vec {
 	return NewVec(b.min.X+x, b.min.Y+y)
+}
+
+func (b *Box) Translate(amount Vec) {
+	b.min = b.min.Add(amount)
+	b.max = b.max.Add(amount)
+}
+
+func (b *Box) ShiftToHold(pos Vec, margin int) {
+	translate := ZeroVec
+
+	leftMargin := pos.X - margin
+	if b.min.X > leftMargin {
+		translate.X = leftMargin - b.min.X
+	} else {
+		rightMargin := pos.X + margin
+		if b.max.X < rightMargin {
+			translate.X = rightMargin - b.max.X
+		}
+	}
+
+	topMargin := pos.Y - margin
+	if b.min.Y > topMargin {
+		translate.Y = topMargin - b.min.Y
+	} else {
+		bottomMargin := pos.Y + margin
+		if b.max.Y < bottomMargin {
+			translate.Y = bottomMargin - b.max.Y
+		}
+	}
+
+	if translate != ZeroVec {
+		b.Translate(translate)
+	}
 }
