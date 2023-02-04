0162f5a60fe589bf3115144b61fed10a36790e9f
Dan Schoenblum <dan.schoenblum@gmail.com>
2023-02-03 21:59:08 -0800

    Add AddMap and CloneMap

--- map.go
+++ map.go
@@ -1,8 +1,22 @@
 package common
 
 func AnyKey[K comparable, V any](m map[K]V) K {
-	for key := range m {
-		return key
+	for k := range m {
+		return k
 	}
 	panic("no keys found")
+}
+
+func AddMap[K comparable, V any](dst map[K]V, src map[K]V) {
+	for k, v := range src {
+		dst[k] = v
+	}
+}
+
+func CloneMap[K comparable, V any](src map[K]V) (dst map[K]V) {
+	dst = make(map[K]V, len(src))
+	for k, v := range src {
+		dst[k] = v
+	}
+	return
 }
