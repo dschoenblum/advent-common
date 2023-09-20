afa06f59137279a8fa891cc2b0f0affcfe4818f1
Dan Schoenblum <dan.schoenblum@gmail.com>
2023-09-20 10:43:25 -0700

    Add some Vec4 methods

--- vec4.go
+++ vec4.go
@@ -14,6 +14,15 @@ func NewVec4(x, y, z, w int) Vec4 {
 	return Vec4{x, y, z, w}
 }
 
+func (v Vec4) Add(other Vec4) Vec4 {
+	return Vec4{
+		X: v.X + other.X,
+		Y: v.Y + other.Y,
+		Z: v.Z + other.Z,
+		W: v.W + other.W,
+	}
+}
+
 func (v Vec4) Sub(other Vec4) Vec4 {
 	return Vec4{
 		X: v.X - other.X,
@@ -33,4 +42,34 @@ func (v Vec4) ManhattanDistance() int {
 
 func (v Vec4) ManhattanDistanceTo(other Vec4) int {
 	return v.Sub(other).ManhattanDistance()
+}
+
+func (v *Vec4) UpdateMin(other Vec4) {
+	if other.X < v.X {
+		v.X = other.X
+	}
+	if other.Y < v.Y {
+		v.Y = other.Y
+	}
+	if other.Z < v.Z {
+		v.Z = other.Z
+	}
+	if other.W < v.W {
+		v.W = other.W
+	}
+}
+
+func (v *Vec4) UpdateMax(other Vec4) {
+	if other.X > v.X {
+		v.X = other.X
+	}
+	if other.Y > v.Y {
+		v.Y = other.Y
+	}
+	if other.Z > v.Z {
+		v.Z = other.Z
+	}
+	if other.W > v.W {
+		v.W = other.W
+	}
 }
