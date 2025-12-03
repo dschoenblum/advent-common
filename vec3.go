3bbf2ef34371241fdcb0fd9a155f1c7ca1f01245
Dan Schoenblum <dan.schoenblum@gmail.com>
2025-12-02 17:31:03 -0800

    Add Vec3.SquaredDistanceTo

--- vec3.go
+++ vec3.go
@@ -51,6 +51,10 @@ func (v Vec3) ManhattanDistanceTo(other Vec3) int {
 	return v.Sub(other).ManhattanDistance()
 }
 
+func (v Vec3) SquaredDistanceTo(other Vec3) int {
+	return v.Sub(other).DistanceSquared()
+}
+
 func (v Vec3) DistanceSquared() int {
 	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
 }
